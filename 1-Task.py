0e216c4dc1585ce8b1d2ba4981272b2634c2c8fd
GerEst <91455094+GerEst@users.noreply.github.com>
2021-09-27 16:21:42 +0300

    Update 1-Task.py

--- 1-Task.py
+++ 1-Task.py
@@ -1,1 +1,16 @@
-
+import turtle
+from random import *
+i = 0
+while i < 300:
+    go = random()
+    angle = randint(-180, 180)
+    turtle.speed(0)
+    turtle.right(angle)
+    turtle.forward(50*go)
+    i+=1
+while i < 300:
+    a = random()
+    b = random()
+    turtle.speed(0)
+    turtle.goto(a, b)
+    i+=1
