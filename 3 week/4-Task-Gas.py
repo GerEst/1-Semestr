71d25d767c2f30d526a102ccb69750b781adf20c
GerEst <91455094+GerEst@users.noreply.github.com>
2021-09-28 17:34:36 +0300

    gotovo

--- 3 week/4-Task-Gas.py
+++ 3 week/4-Task-Gas.py
@@ -12,16 +12,16 @@ while zabor < 4:
     zabor += 1
 turtle.ht()
 
-number_of_particles = 5
-steps_of_time_number = 300
+number_of_particles = 1
+steps_of_time_number = 500
 spped = 5 # скорость частицы
 
 pool = [turtle.Turtle(shape = 'circle') for i in range(number_of_particles)]
 
 # генерируем координаты частиц
-Xc = []
-Yc = []
-A  = []
+Xc = [] # x koordinata
+Yc = [] # y koordinata
+A  = [] # ugol dvizenija
 
 for unit in pool:
     unit.penup()
@@ -31,7 +31,7 @@ for unit in pool:
     Xc.append(x)
     Yc.append(y)
     unit.goto(x,y)
-    alpha = randint(-180 , 180)
+    alpha = randint(-180, 180)
     unit.right(alpha)
     A.append(alpha)
 
@@ -39,18 +39,22 @@ print(Xc)
 print(Yc)
 print(A)
 
+turtle.tracer()
+
 for i in range(steps_of_time_number):
     for unit in pool:
-        if abs(unit.xcor()) < 200:
-            if abs(unit.ycor()) < 200:
+        if abs(unit.xcor()) <= 200:
+            if abs(unit.ycor()) <= 200:
                 unit.speed(0)
-                unit.forward(4)
+                unit.forward(3)
             else:
-                unit.right(90 - 2*A[pool.index(unit)]) #реализовать поворот
+                unit.right(360 - 2*A[pool.index(unit)]) # удар об y
+                #A[pool.index(unit)] = 180 - 2*A[pool.index(unit)]
                 unit.speed(0)
-                unit.forward(4)
+                unit.forward(3)
         else:
-            unit.right(180 -2*A[pool.index(unit)]) #реализовать поворот
+            unit.right(180 - 2*A[pool.index(unit)]) # удар об x
+            #A[pool.index(unit)] = 360 -2*A[pool.index(unit)]
             unit.speed()
-            unit.forward(4)
+            unit.forward(3)
             # реализовать поворот
