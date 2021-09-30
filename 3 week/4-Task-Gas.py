38d7124da1794db8041d3d6a1a7d2250058730ee
German Lužin <91455094+GerEst@users.noreply.github.com>
2021-09-30 19:40:31 +0300

    Update 4-Task-Gas.py

--- 3 week/4-Task-Gas.py
+++ 3 week/4-Task-Gas.py
@@ -2,16 +2,18 @@ from random import randint
 import turtle
 
 #draw pool
+r = 200
 turtle.penup()
-turtle.goto(-200, 200)
+turtle.goto(-r, r)
 turtle.pendown()
 zabor = 0
 while zabor < 4:
-    turtle.forward(400)
+    turtle.forward(2*r)
     turtle.right(90)
     zabor += 1
 turtle.ht()
 
+ 
 number_of_particles = 4
 steps_of_time_number = 500
 V = 20 # скорость частицы
@@ -26,8 +28,8 @@ A  = [] # ugol dvizenija
 for unit in pool:
     unit.penup()
     unit.speed(50)
-    x = randint(-200, 200)
-    y = randint(-200, 200)
+    x = randint(-r, r)
+    y = randint(-r, r)
     Xc.append(x)
     Yc.append(y)
     unit.goto(x,y)
@@ -43,8 +45,8 @@ turtle.tracer()
 
 for i in range(steps_of_time_number):
     for unit in pool:
-        if abs(unit.xcor()) <= 200:
-            if abs(unit.ycor()) <= 200:
+        if abs(unit.xcor()) <= r:
+            if abs(unit.ycor()) <= r:
                 unit.speed(0)
                 unit.forward(V)
             else:
@@ -53,8 +55,6 @@ for i in range(steps_of_time_number):
                 unit.speed(0)
                 unit.forward(V)
         else:
-            unit.setheading(180 - unit.heading()) # удар об горизонт. стенки
-            #A[pool.index(unit)] = 360 -2*A[pool.index(unit)]
+            unit.setheading(180 - unit.heading()) 
             unit.speed(0)
             unit.forward(V)
-            # реализовать поворот
