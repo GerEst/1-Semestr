53ae4bf0aaa94c4dfb2d9ee827ecb5c5e2eee797
GerEst <91455094+GerEst@users.noreply.github.com>
2021-11-16 18:02:59 +0200

    создал класс Ball

---
+++ 5 week/Ball.py
@@ -0,0 +1,46 @@
+import pygame
+from Constantine import *
+from random import randint
+
+
+
+class Ball:
+    def __init__(self):
+        self.x = randint(100,700)
+        self.y = randint(100,500)
+        self.r = randint(30,50)
+
+        self.Vx = randint(-10,10)
+        self.Vy = randint(-10,10)
+
+        self.color = COLORS[randint(0, 5)]
+
+
+    def render(self, screen):
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.r)
+
+    def move(self):
+        self.x += self.Vx
+        self.y += self.Vy
+
+        if self.x + self.r > SCREEN_WIDTH:
+            self.Vx = randint(-10, 0)
+            self.Vy = randint(-10, 10)
+
+        if self.x - self.r < 0:
+            self.Vx = randint(0, 10)
+            self.Vy = randint(-10, 10)
+
+
+        if self.y + self.r > SCREEN_HEIGHT:
+            self.Vx = randint(-10, 10)
+            self.Vy = randint(-10, 0)
+        if self.y - self.r < 0:
+            self.Vx = randint(-10, -10)
+            self.Vy = randint(0, 10)
+
+
+
+
+
+ball1 = Ball()
